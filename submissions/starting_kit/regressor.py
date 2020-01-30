ff256f8fc676cbfb4525e4e7bf8927a6fac5b6b0
WissamGherissi <60392709+WissamGherissi@users.noreply.github.com>
2020-01-30 18:40:44 +0100

    Update regressor.py

--- submissions/starting_kit/regressor.py
+++ submissions/starting_kit/regressor.py
@@ -2,15 +2,17 @@ from sklearn import linear_model
 from sklearn.base import BaseEstimator
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.rgr = make_pipeline(StandardScaler(), linear_model.Ridge(alpha=.5))  #Avec scaling
-
+        self.rgr = make_pipeline(StandardScaler(),RandomForestRegressor(n_estimators=20,max_depth=30))
+        
     def fit(self, X, y):
-        self.rgr.fit(X, y.reshape((len(y), 1)))
+        self.rgr.fit(X, y.reshape(len(y),1))
 
     def predict(self, X):
-        y_pred = self.rgr.predict(X)
-        return y_pred.reshape((len(y_pred), 1))
+        y_pred =  self.rgr.predict(X)
+        return y_pred.reshape(len(y_pred),1)
+
