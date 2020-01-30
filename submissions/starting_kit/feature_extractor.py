359d343aec2086ca0975b768969ba0113620e753
WissamGherissi <60392709+WissamGherissi@users.noreply.github.com>
2020-01-30 19:10:31 +0100

    Update feature_extractor.py

--- submissions/starting_kit/feature_extractor.py
+++ submissions/starting_kit/feature_extractor.py
@@ -1,31 +1,30 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer, make_column_transformer
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.preprocessing import OrdinalEncoder, FunctionTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.compose import make_column_transformer
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import make_pipeline
+import category_encoders as ce
+import pandas as pd
+import numpy as np
 
 
-numeric_cols = [c for c in suicide if suicide[c].dtype.kind in ('i', 'f')]
-categorical_cols = [c for c in suicide if suicide[c].dtype.kind not in ('i', 'f')]
-drop_cols = ['country-year']
+class FeatureExtractor(object):
+    def __init__(self):
+        pass
 
+    def fit(self, X_df, y_array):
+        pass
+
+    def transform(self, X_df):
+        X_new = X_df.copy()
+        numeric_cols = [c for c in X_new if X_new[c].dtype.kind in ('i', 'f') ]
+        drop_cols = ['country-year','continent']
+        ct = ColumnTransformer(transformers=[('numeric', numeric_transformer, numeric_cols),
+          ('binary', OrdinalEncoder(), ['sex','age','generation']),                                 
+          ('drop cols', 'drop', drop_cols),
+         ])
+
+        XX = ct.fit_transform(X_new)
+        return XX
+        
 numeric_transformer = Pipeline(steps=[('impute', SimpleImputer(strategy='median'))])
-
-cat_encoder = make_column_transformer((OrdinalEncoder(), categorical_cols), remainder='passthrough')
-
-
-def process_gdp_for_year(X):
-    gdp = X[' gdp_for_year ($)'].str[:-12]
-    return pd.to_numeric(gdp, errors='coerce').values[:, np.newaxis]
-
-gdp_year_transformer = FunctionTransformer(process_gdp_for_year, validate=False)
-   
-
-ct = ColumnTransformer(
-    transformers=[('numeric', numeric_transformer, numeric_cols),
-                  ('categoric', cat_encoder, categorical_cols),
-                  ('gdpforyear', gdp_year_transformer,['gdp_for_year ($)']),
-                  ('drop cols', 'drop', drop_cols),
-                 ])
